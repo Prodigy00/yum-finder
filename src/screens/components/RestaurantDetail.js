c50fdf2abdb30374c0725e476d45bf11e48bbc70
Prodigy00 <simplygiddy@gmail.com>
2019-12-24 21:17:08 +0100

    fixed flatlist issue, loaded images from api

--- src/screens/components/RestaurantDetail.js
+++ src/screens/components/RestaurantDetail.js
@@ -4,11 +4,24 @@ import { Text, View, Image, StyleSheet } from "react-native";
 const RestaurantDetail = ({ restaurant }) => {
   return (
     <View>
-      <Text>{restaurant.name}</Text>
+      <Image style={styles.image} source={{ uri: restaurant.image_url }} />
+      <Text style={styles.name}>{restaurant.name}</Text>
+      <Text>
+        {restaurant.rating} Stars, {restaurant.review_count} Reviews
+      </Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  },
+  name: {}
+});
 
 export default RestaurantDetail;
+
+//native image components won't show anything until you give them a height and width > 0
