eb82adae909a28858b30dd798698fc4e8f964846
Prodigy00 <simplygiddy@gmail.com>
2019-12-24 19:03:50 +0100

    created useRestaurants hooks

---
+++ src/hooks/useRestaurants.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import yelp from "../api/yelp";
+
+export default () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const searchApi = async searchTerm => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose"
+        }
+      });
+      setRestaurants(response.data.businesses);
+    } catch (error) {
+      console.log(error);
+      setErrorMessage("Something went wrong...");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+
+  return [searchApi, restaurants, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,31 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "./components/SearchBar";
-import yelp from "../api/yelp";
+import useRestaurants from "../hooks/useRestaurants";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [restaurants, setRestaurants] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-  const searchApi = async searchTerm => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term,
-          location: "san jose"
-        }
-      });
-      setRestaurants(response.data.businesses);
-    } catch (error) {
-      console.log(error);
-      setErrorMessage("Something went wrong...");
-    }
-  };
-
-  useEffect(() => {
-    searchApi("pasta");
-  }, []);
+  const [searchApi, restaurants, errorMessage] = useRestaurants();
 
   return (
     <View>
@@ -35,7 +15,7 @@ const SearchScreen = () => {
         onTermChange={setTerm}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {restaurants.length} results</Text>
+      <Text>We have found {restaurants.length} restaurants</Text>
     </View>
   );
 };
