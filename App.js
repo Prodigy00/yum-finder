518a0327cd4fac560bba1022b6a715a0477bb5c4
Prodigy00 <simplygiddy@gmail.com>
2019-12-24 16:45:35 +0100

    added search bar

--- App.js
+++ App.js
@@ -2,20 +2,18 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import SearchScreen from "./src/screens/SearchScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen
+  },
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Business Search"
+    }
   }
-});
+);
+
+export default createAppContainer(navigator);
---
+++ src/screens/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+
+export default SearchBar;
