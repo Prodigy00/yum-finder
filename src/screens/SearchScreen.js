6456db0bc21c8d1e2886a3839c470ab641c039f4
Prodigy00 <simplygiddy@gmail.com>
2019-12-24 18:49:34 +0100

    added async call to searchScreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "./components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const searchApi = async searchTerm => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "san jose"
+        }
+      });
+      setRestaurants(response.data.businesses);
+    } catch (error) {
+      console.log(error);
+      setErrorMessage("Something went wrong...");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+
   return (
     <View>
       <SearchBar
-        onTermSubmit={() => console.log("Submitted")}
+        onTermSubmit={() => searchApi(term)}
         term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
+        onTermChange={setTerm}
       />
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {restaurants.length} results</Text>
     </View>
   );
 };
