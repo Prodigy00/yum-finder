e2b9eeb5db0e4525504711ae072453599eb66b78
Prodigy00 <simplygiddy@gmail.com>
2019-12-25 14:43:55 +0100

    removed navigation prop

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,7 @@ import SearchBar from "./components/SearchBar";
 import useRestaurants from "../hooks/useRestaurants";
 import RestaurantsList from "./components/RestaurantsList";
 
-const SearchScreen = ({ navigation }) => {
+const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, restaurants, errorMessage] = useRestaurants();
   //   console.log({ restaurants });
@@ -28,17 +28,14 @@ const SearchScreen = ({ navigation }) => {
         <RestaurantsList
           restaurants={filterResultsByPrice("$")}
           title="Cost Effective"
-          navigation={navigation}
         />
         <RestaurantsList
           restaurants={filterResultsByPrice("$$")}
           title="Bit Pricier"
-          navigation={navigation}
         />
         <RestaurantsList
           restaurants={filterResultsByPrice("$$$")}
           title="Big Spender"
-          navigation={navigation}
         />
       </ScrollView>
     </>
