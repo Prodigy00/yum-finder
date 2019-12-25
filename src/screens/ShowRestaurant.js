970e1810fc07a80786f0234f85ba7c9dbe7653b6
Prodigy00 <simplygiddy@gmail.com>
2019-12-25 15:29:26 +0100

    added flatlist to showRestaurants

--- src/screens/ShowRestaurant.js
+++ src/screens/ShowRestaurant.js
@@ -1,16 +1,43 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
-
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, FlatList, Image } from "react-native";
+import yelp from "../api/yelp";
 const showRestaurant = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam("id");
-  console.log({ id });
+
+  const getResult = async id => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
+  const renderItem = ({ item }) => {
+    return <Image style={styles.image} source={{ uri: item }} />;
+  };
+
   return (
     <View>
-      <Text>Show Restaurant</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={photo => photo}
+        renderItem={renderItem}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300
+  }
+});
 
 export default showRestaurant;
